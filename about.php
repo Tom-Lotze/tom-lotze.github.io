8bc34b626301d97c3d1aaa49e61e691125e811fa
Tom Lotze <tom.lotze@student.auc.nl>
2020-01-14 19:46:43 +0100

    testing with php

---
+++ about.php
@@ -0,0 +1,38 @@
+<!doctype=html>
+<html lan="en">
+<head>
+    <meta charset="utf-8">
+    <title>About</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+
+<div class="header">
+  <h1>Tom Lotze</h1>
+</div>
+
+ <ul>
+  <li><a href="index.php">Tom Lotze</a></li>
+  <li><a class='active' href="about.php">About</a></li>
+  <li><a href="contact.php">Contact</a></li>
+</ul>
+
+<body>
+  <h2>Education</h1>
+    <p>
+       I am currently a Master student <a href="https://www.uva.nl/en/programmes/masters/artificial-intelligence/artificial-intelligence.html"> Artificial Intelligence</a> at the University of Amsterdam. I expect to graduate from this program in July 2021. My interests lie in Deep learning, Computer Vision and Natural Language processing. 
+    </p>
+  <h2>Projects</h1>
+  <p>
+    Please take a look at my Github to see the projects I have worked on.
+  </p>
+
+  <?php    include './footer.php';
+  ?>
+
+<!-- <div class="footer">
+  <a href="https://linkedin.com/in/tom-lotze/"><img src="media/Linkedin_logo.png" width="60" height="60"></a>
+</div> -->
+</body>
+</html>
+
---
+++ navbar.php
@@ -0,0 +1,8 @@
+<?php
+echo 
+ <ul>
+  <li><a class='active' href="index.html">Tom Lotze</a></li>
+  <li><a href="about.html">About</a></li>
+  <li><a href="contact.html">Contact</a></li>
+</ul>
+?>
